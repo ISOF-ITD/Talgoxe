f0d18d4a95667a22852b05752ea590c7d0b83a35
bjkar031 <bjornk.karlsson@sprakochfolkminnen.se>
2018-11-27 19:31:14 +0100

    OS-409 First version of new menu.

--- talgoxe/urls.py
+++ talgoxe/urls.py
@@ -6,14 +6,14 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
-    url('clipboard', views.clipboard, name = 'clipboard'),
-    url('create', views.create, name = 'create'),
+    url(r'^clipboard', views.clipboard, name = 'clipboard'),
+    url(r'^create', views.create, name = 'create'),
     url(r'^delete/(?P<id>\d+)$', views.delete, name = 'delete'),
     url(r'^redigera/(?P<id>\d+)$', views.redigera, name = 'redigera'),
     url(r'^stickord/(?P<stickord>.+)$', views.artikel_efter_stickord, name = 'stickord'),
     url(r'^search$', views.search, name = 'search'),
     url(r'^artikel/(?P<id>\d+)?$', views.artikel, name = 'artikel'),
-    url(r'print-on-demand$', views.print_on_demand, name = 'print_on_demand'),
-    url(r'print-on-demand/(?P<format>.*)', views.print, name = 'print'),
-    url(r'logout', views.easylogout, name = 'easylogout'),
+    url(r'^print-on-demand$', views.print_on_demand, name = 'print_on_demand'),
+    url(r'^print-on-demand/(?P<format>.*)', views.print, name = 'print'),
+    url(r'^logout', views.easylogout, name = 'easylogout'),
 ]
