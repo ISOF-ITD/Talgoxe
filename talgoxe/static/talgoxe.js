e8a68e185b2a2485a77c36e700205fbd62558a40
bjkar031 <bjornk.karlsson@sprakochfolkminnen.se>
2019-05-08 16:40:48 +0200

    Attempt to solve problems reported bu user. No change in data handling.

--- talgoxe/static/talgoxe.js
+++ talgoxe/static/talgoxe.js
@@ -407,10 +407,6 @@ $(document).ready(function() {
         articleItemCount = 0;
     }
 
-    $('#inmatning').on( 'change keyup keydown paste cut', 'textarea', function (){
-        $(this).height(0).height(this.scrollHeight);
-    }).find( 'textarea' ).change();
-
     // Start of code copied from https://www.brainbell.com/javascript/making-resizable-table-js.html
     // The code is used to create resizable tables.
     var tables = document.getElementsByTagName('table');
@@ -506,6 +502,10 @@ $(document).ready(function() {
          }
     };
     // End of code copied from https://www.brainbell.com/javascript/making-resizable-table-js.html
+
+    $('#inmatning').on( 'change keyup keydown paste cut', 'textarea', function (){
+        $(this).height(0).height(this.scrollHeight);
+    }).find( 'textarea' ).change();
 
     $('.add-first-row').click(addFirstArticleItem);
     $('.add-row').click(addArticleItem);
